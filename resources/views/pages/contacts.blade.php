d572df8944ed6d2fa5a1668fe79741714d3a07af
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-10-13 21:51:59 +0800

    added more db

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -6,7 +6,7 @@ del sito la posizioniamo nella cartella views assieme alla Home--}}
 
 @section('title', 'Contacts')
 {{--cdns--}}
-@section('cdns')    {{--per importare font awesome sono in questa pagina usiamo questa procedura @section--}}
+@section('cdns')    {{--per importare font awesome solo in questa pagina usiamo questa procedura @section--}}
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
 
--- resources/views/travels/index.blade.php
+++ resources/views/travels/index.blade.php
@@ -4,17 +4,28 @@
 @section('title', 'Our travels')
 
 @section('content')
+
     <section id="travels" class="container">
         <h1 class="text-center my-5">Viaggi in tutto il mondo</h1>
-        <div class="card my-5" style="width: 18rem;">
+        @foreach ($travels as $travel)
+        <div class="card my-5 d-flex">
             <div class="card-body">
                 {{--problema di risolvere con inportare i dati da DB che non li trova--}}
-              <h5 class="card-title">{{$travels['id']}}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+              
+              <h5 class="card-title">{{$travel->country}}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{$travel->region}}</h6>
+              <h6 class="card-subtitle mb-2 text-muted">{{$travel->province}}</h6>
+              <h6 class="card-subtitle mb-2 text-muted">{{$travel->city}}</h6>
+              <h6 class="card-subtitle mb-2 text-muted">{{$travel->address}}</h6>
+              <h6 class="card-subtitle mb-2 text-muted">{{$travel->postal_code}}</h6>
+              <span>{{$travel->start_date}}</span>
+              <span>{{$travel->end_date}}</span>
+
+              <p class="card-text">{{$travel->description}}</p>
+              
+                
             </div>
           </div>
+          @endforeach
     </section>
 @endsection
